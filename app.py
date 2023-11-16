9cec21a1b451cfe08baf7a51aaf9f1fd83900ac4
Fahrudin Mohamad <mohamad.fahrudin18@gmail.com>
2023-11-16 23:36:51 +0700

    init

---
+++ app.py
@@ -0,0 +1,124 @@
+#importing required libraries
+
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+import warnings
+import pickle
+warnings.filterwarnings('ignore')
+from feature import FeatureExtraction
+import joblib
+
+def is_phishing_gbc(x):
+    #LOAD PICKLE
+    file_gbc = r"model\gbc_model.pkl"
+    gbc = joblib.load(file_gbc)
+
+    # PREDIKSI GRADIENT BOOST
+    gbc_prediction = gbc.predict(x)
+    gbc_safe_probability = round(gbc.predict_proba(x)[0,1] * 100, 2)
+    gbc_malicious_probability = round(gbc.predict_proba(x)[0,0] * 100, 2)
+
+    y_test_gbc = gbc.predict(X_test)
+    acc_test_gbc = accuracy_score(y_test,y_test_gbc) * 100
+
+    return {
+        "gbc_prediction": gbc_prediction,
+        "gbc_safe_probability": gbc_safe_probability,
+        "gbc_malicious_probability": gbc_malicious_probability,
+        "gbc_acc_test": acc_test_gbc
+    }
+
+def is_phishing_cat(x):
+    file_cat = r"model\cat_model.pkl"
+    cat = joblib.load(file_cat)
+
+    # PREDIKSI CATBOOST
+    cat_prediction = cat.predict(x)
+    cat_safe_probability = round(cat.predict_proba(x)[0,1] * 100, 2)
+    cat_malicious_probability = round(cat.predict_proba(x)[0,0] * 100, 2)
+
+    y_test_cat = cat.predict(X_test)
+    acc_test_cat = accuracy_score(y_test,y_test_cat) * 100
+
+    return {
+        "cat_prediction": cat_prediction,
+        "cat_safe_probability": cat_safe_probability,
+        "cat_malicious_probability": cat_malicious_probability,
+        "cat_acc_test": acc_test_cat
+    }
+
+def is_phishing_mlp(x):
+    file_mlp = r"model\mlp_model.pkl"
+    mlp = joblib.load(file_mlp)
+
+    # PREDIKSI MULTI-LAYER
+    mlp_prediction = mlp.predict(x)
+    mlp_safe_probability = round(mlp.predict_proba(x)[0,1] * 100 ,2)
+    mlp_malicious_probability = round(mlp.predict_proba(x)[0,0] * 100 , 2)
+
+    y_test_mlp = mlp.predict(X_test)
+    acc_test_mlp = accuracy_score(y_test,y_test_mlp) * 100
+
+    return {
+        "mlp_prediction": mlp_prediction,
+        "mlp_safe_probability": mlp_safe_probability,
+        "mlp_malicious_probability": mlp_malicious_probability,
+        "mlp_acc_test": acc_test_mlp
+    }
+
+def Load_Data():
+    global X_test, y_test
+
+    X_test_file = r"model\X_test.pkl"
+    X_test = joblib.load(X_test_file)
+
+    y_test_file = r"model\y_test.pkl"
+    y_test = joblib.load(y_test_file)
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        Load_Data()
+
+        url = request.form["url"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30) 
+
+        gbc_result = is_phishing_gbc(x)
+        cat_result = is_phishing_cat(x)
+        mlp_result = is_phishing_mlp(x)
+        
+        chart = True
+
+        return render_template('index.html',**gbc_result, **cat_result,**mlp_result ,url=url, chart=chart)
+    else:
+        gbc_result = {
+            "gbc_prediction": -1,
+            "gbc_safe_probability": -1,
+            "gbc_malicious_probability": -1,
+            "gbc_acc_test": -1
+        }
+        cat_result = {
+            "cat_prediction": -1,
+            "cat_safe_probability": -1,
+            "cat_malicious_probability": -1,
+            "cat_acc_test": -1
+        }
+        mlp_result = {
+            "mlp_prediction": -1,
+            "mlp_safe_probability": -1,
+            "mlp_malicious_probability": -1,
+            "mlp_acc_test": -1
+        }
+        
+        chart = False
+        return render_template("index.html", **gbc_result, **cat_result, **mlp_result, chart=chart)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=2002)
---
+++ feature.py
@@ -0,0 +1,513 @@
+import ipaddress
+import re
+import urllib.request
+from bs4 import BeautifulSoup
+import socket
+import requests
+import whois
+from datetime import date, datetime
+import time
+from dateutil.parser import parse as date_parse
+from urllib.parse import urlparse
+import ssl
+from cryptography import x509
+from urllib import response
+
+class FeatureExtraction:
+    features = []
+    def __init__(self,url):
+        self.features = []
+        self.url = url
+        self.domain = ""
+        self.whois_response = ""
+        self.urlparse = ""
+        self.response = ""
+        self.soup = ""
+
+        try:
+            self.response = requests.get(url)
+            self.soup = BeautifulSoup(response.text, 'html.parser')
+        except:
+            pass
+
+        try:
+            self.urlparse = urlparse(url)
+            self.domain = self.urlparse.netloc
+        except:
+            pass
+
+        try:
+            self.whois_response = whois.whois(self.domain)
+        except:
+            pass
+
+
+
+
+        self.features.append(self.Having_IP_Address())
+        self.features.append(self.URL_Length())
+        self.features.append(self.Shortining_Service())
+        self.features.append(self.having_At_Symbol())
+        self.features.append(self.double_slash_redirecting())
+        self.features.append(self.Prefix_Suffix())
+        self.features.append(self.having_Sub_Domain())
+        self.features.append(self.SSLfinal_State())
+        self.features.append(self.Domain_registeration_length())
+        self.features.append(self.Favicon())
+
+        self.features.append(self.port())
+        self.features.append(self.HTTPS_token())
+        self.features.append(self.Request_URL())
+        self.features.append(self.URL_of_Anchor())
+        self.features.append(self.Links_in_tags())
+        self.features.append(self.SFH())
+        self.features.append(self.Submitting_to_email())
+        self.features.append(self.Abnormal_URL())
+        self.features.append(self.Redirect())
+        self.features.append(self.on_mouseover())
+
+        self.features.append(self.RightClick())
+        self.features.append(self.popUpWidnow())
+        self.features.append(self.Iframe())
+        self.features.append(self.age_of_domain())
+        self.features.append(self.DNSRecord())
+        self.features.append(self.web_traffic())
+        self.features.append(self.Page_Rank())
+        self.features.append(self.Google_Index())
+        self.features.append(self.Links_pointing_to_page())
+        self.features.append(self.Statistical_report())
+
+
+     # 1.Having_IP_Address
+    def Having_IP_Address(self):
+        try:
+            ipaddress.ip_address(self.url)
+            return -1
+        except:
+            return 1
+
+    # 2.URL_Length
+    def URL_Length(self):
+        if len(self.url) < 54:
+            return 1
+        if len(self.url) >= 54 and len(self.url) <= 75:
+            return 0
+        return -1
+
+    # 3.Shortining_Service
+    def Shortining_Service(self):
+        match = re.search('bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|'
+                    'yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|'
+                    'short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|'
+                    'doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|'
+                    'db\.tt|qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|'
+                    'q\.gs|is\.gd|po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|'
+                    'x\.co|prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|tr\.im|link\.zip\.net', self.url)
+        if match:
+            return -1
+        return 1
+
+    # 4.having_At_Symbol
+    def having_At_Symbol(self):
+        if re.findall("@",self.url):
+            return -1
+        return 1
+
+    # 5.double_slash_redirecting
+    def double_slash_redirecting(self):
+        if self.url.rfind('//')>6:
+            return -1
+        return 1
+
+    # 6.Prefix_Suffix
+    def Prefix_Suffix(self):
+        try:
+            match = re.findall('\-', self.domain)
+            if match:
+                return -1
+            return 1
+        except:
+            return -1
+
+    # 7.having_Sub_Domain
+    def having_Sub_Domain(self):
+        dot_count = len(re.findall("\.", self.url))
+        if dot_count == 1:
+            return 1
+        elif dot_count == 2:
+            return 0
+        return -1
+
+    # 8.SSLfinal_State
+    def SSLfinal_State(self):
+        try:
+            domain = self.domain
+
+            ctx = ssl.create_default_context()
+            with ctx.wrap_socket(socket.socket(), server_hostname=domain) as s:
+                s.connect((domain, 443))
+                cert = s.getpeercert()
+
+            issuer = dict(x[0] for x in cert['issuer'])
+            issued_by = issuer['organizationName'].strip()
+
+            # Check if the URL uses HTTPS
+            if self.url.startswith('https://'):
+                # Check if the issuer is trusted
+                trusted_issuers = ['DigiCert Inc','DigiCert, Inc.', 'Google Trust Services LLC','Let\'s Encrypt','GlobalSign','Entrust Datacard Corporation','Comodo CA Limited','Sectigo Limited',
+                                   'GoDaddy.com, LLC.','Trustwave Holdings, Inc.','CSC Security Solutions, Inc.','Network Solutions LLC']  # Trusted issuers organization name
+                if issued_by in trusted_issuers:
+                  print("Valid HTTPS, and trusted issuer")
+                  return 1
+                else:
+                  print("Valid HTTPS, but issuer is not trusted")
+                  return 0
+            else:
+                print("URL does not use HTTPS")
+                return -1
+
+        except Exception as e:
+            print(e)
+            return -1
+
+
+    # 9.Domain_registeration_length
+    def Domain_registeration_length(self):
+        try:
+            expiration_date = self.whois_response.expiration_date
+            creation_date = self.whois_response.creation_date
+            try:
+                if(len(expiration_date)):
+                    expiration_date = expiration_date[0]
+            except:
+                pass
+            try:
+                if(len(creation_date)):
+                    creation_date = creation_date[0]
+            except:
+                pass
+
+            age = (expiration_date.year-creation_date.year)*12+ (expiration_date.month-creation_date.month)
+            if age >=12:
+                return 1
+            return -1
+        except:
+            return -1
+
+    # 10. Favicon
+    def Favicon(self):
+        try:
+            for head in self.soup.find_all('head'):
+                for head.link in self.soup.find_all('link', href=True):
+                    dots = [x.start(0) for x in re.finditer('\.', head.link['href'])]
+                    if self.url in head.link['href'] or len(dots) == 1 or self.domain in head.link['href']:
+                        return 1
+            return -1
+        except:
+            return -1
+
+    # 11. port
+    def port(self):
+        try:
+            port = self.domain.split(":")
+            if len(port)>1:
+                return -1
+            return 1
+        except:
+            return -1
+
+    # 12. HTTPS_token
+    def HTTPS_token(self):
+        try:
+            if 'https' in urlparse(self).netloc:
+                return -1
+            return 1
+        except Exception as e:
+            return -1
+
+    # 13. Request_URL
+    def Request_URL(self):
+        try:
+            for img in self.soup.find_all('img', src=True):
+                dots = [x.start(0) for x in re.finditer('\.', img['src'])]
+                if self.url in img['src'] or self.domain in img['src'] or len(dots) == 1:
+                    success = success + 1
+                i = i+1
+
+            for audio in self.soup.find_all('audio', src=True):
+                dots = [x.start(0) for x in re.finditer('\.', audio['src'])]
+                if self.url in audio['src'] or self.domain in audio['src'] or len(dots) == 1:
+                    success = success + 1
+                i = i+1
+
+            for embed in self.soup.find_all('embed', src=True):
+                dots = [x.start(0) for x in re.finditer('\.', embed['src'])]
+                if self.url in embed['src'] or self.domain in embed['src'] or len(dots) == 1:
+                    success = success + 1
+                i = i+1
+
+            for iframe in self.soup.find_all('iframe', src=True):
+                dots = [x.start(0) for x in re.finditer('\.', iframe['src'])]
+                if self.url in iframe['src'] or self.domain in iframe['src'] or len(dots) == 1:
+                    success = success + 1
+                i = i+1
+
+            try:
+                percentage = success/float(i) * 100
+                if percentage < 22.0:
+                    return 1
+                elif((percentage >= 22.0) and (percentage < 61.0)):
+                    return 0
+                else:
+                    return -1
+            except:
+                return 0
+        except:
+            return -1
+
+    # 14. URL_of_Anchor
+    def URL_of_Anchor(self):
+        try:
+            i,unsafe = 0,0
+            for a in self.soup.find_all('a', href=True):
+                if "#" in a['href'] or "javascript" in a['href'].lower() or "mailto" in a['href'].lower() or not (self.url in a['href'] or self.domain in a['href']):
+                    unsafe = unsafe + 1
+                i = i + 1
+
+            try:
+                percentage = unsafe / float(i) * 100
+                if percentage < 31.0:
+                    return 1
+                elif ((percentage >= 31.0) and (percentage < 67.0)):
+                    return 0
+                else:
+                    return -1
+            except:
+                return -1
+
+        except:
+            return -1
+
+    # 15. Links_in_tags
+    def Links_in_tags(self):
+        try:
+            i,success = 0,0
+
+            for link in self.soup.find_all('link', href=True):
+                dots = [x.start(0) for x in re.finditer('\.', link['href'])]
+                if self.url in link['href'] or self.domain in link['href'] or len(dots) == 1:
+                    success = success + 1
+                i = i+1
+
+            for script in self.soup.find_all('script', src=True):
+                dots = [x.start(0) for x in re.finditer('\.', script['src'])]
+                if self.url in script['src'] or self.domain in script['src'] or len(dots) == 1:
+                    success = success + 1
+                i = i+1
+
+            try:
+                percentage = success / float(i) * 100
+                if percentage < 17.0:
+                    return 1
+                elif((percentage >= 17.0) and (percentage < 81.0)):
+                    return 0
+                else:
+                    return -1
+            except:
+                return 0
+        except:
+            return -1
+
+    # 16. SFH
+    def SFH(self):
+        try:
+            if len(self.soup.find_all('form', action=True))==0:
+                return 1
+            else :
+                for form in self.soup.find_all('form', action=True):
+                    if form['action'] == "" or form['action'] == "about:blank":
+                        return -1
+                    elif self.url not in form['action'] and self.domain not in form['action']:
+                        return 0
+                    else:
+                        return 1
+        except:
+            return -1
+
+    # 17. Submitting_to_email
+    def Submitting_to_email(self):
+        try:
+            if re.findall(r"[mail\(\)|mailto:?]", self.soap):
+                return -1
+            else:
+                return 1
+        except:
+            return -1
+
+    # 18. Abnormal_URL
+    def Abnormal_URL(self):
+        try:
+            if self.response.text == self.whois_response:
+                return 1
+            else:
+                return -1
+        except:
+            return -1
+
+    # 19. Redirect
+    def Redirect(self):
+        try:
+            if len(self.response.history) <= 1:
+                return 1
+            elif len(self.response.history) <= 4:
+                return 0
+            else:
+                return -1
+        except:
+             return -1
+
+    # 20. on_mouseover
+    def on_mouseover(self):
+        try:
+            if re.findall("<script>.+onmouseover.+</script>", self.response.text):
+                return 1
+            else:
+                return -1
+        except:
+             return -1
+
+    # 21. RightClick
+    def RightClick(self):
+        try:
+            if re.findall(r"event.button ?== ?2", self.response.text):
+                return 1
+            else:
+                return -1
+        except:
+             return -1
+
+    # 22. popUpWidnow
+    def popUpWidnow(self):
+        try:
+            if re.findall(r"alert\(", self.response.text):
+                return 1
+            else:
+                return -1
+        except:
+             return -1
+
+    # 23. Iframe
+    def Iframe(self):
+        try:
+            if re.findall(r"[<iframe>|<frameBorder>]", self.response.text):
+                return 1
+            else:
+                return -1
+        except:
+             return -1
+
+    # 24. age_of_domain
+    def age_of_domain(self):
+        try:
+            creation_date = self.whois_response.creation_date
+            try:
+                if(len(creation_date)):
+                    creation_date = creation_date[0]
+            except:
+                pass
+
+            today  = date.today()
+            age = (today.year-creation_date.year)*12+(today.month-creation_date.month)
+            if age >=6:
+                return 1
+            return -1
+        except:
+            return -1
+
+    # 25. DNSRecord
+    def DNSRecord(self):
+        try:
+            creation_date = self.whois_response.creation_date
+            try:
+                if(len(creation_date)):
+                    creation_date = creation_date[0]
+            except:
+                pass
+
+            today  = date.today()
+            age = (today.year-creation_date.year)*12+(today.month-creation_date.month)
+            if age >=6:
+                return 1
+            return -1
+        except:
+            return -1
+
+    # 26. web_traffic
+    def web_traffic(self):
+        try:
+            rank = BeautifulSoup(urllib.request.urlopen("http://data.alexa.com/data?cli=10&dat=s&url=" + self.url).read(), "xml").find("REACH")['RANK']
+            if (int(rank) < 100000):
+                return 1
+            return 0
+        except :
+            return -1
+
+    # 27. Page_Rank
+    def Page_Rank(self):
+        try:
+            rank_checker_response = requests.post("https://www.checkpagerank.net/index.php", {"name": self.domain})
+
+            global_rank = int(re.findall(r"Global Rank: ([0-9]+)", rank_checker_response.text)[0])
+            if global_rank > 0 and global_rank < 100000:
+                return 1
+            return -1
+        except:
+            return -1
+
+
+    # 28. Google_Index
+    def Google_Index(self):
+        try:
+            site = search(self.url, 5)
+            if site:
+                return 1
+            else:
+                return -1
+        except:
+            return 1
+
+    # 29. Links_pointing_to_page
+    def Links_pointing_to_page(self):
+        try:
+            number_of_links = len(re.findall(r"<a href=", self.response.text))
+            if number_of_links == 0:
+                return 1
+            elif number_of_links <= 2:
+                return 0
+            else:
+                return -1
+        except:
+            return -1
+
+    # 30. Statistical_report
+    def Statistical_report(self):
+        try:
+            url_match = re.search(
+        'at\.ua|usa\.cc|baltazarpresentes\.com\.br|pe\.hu|esy\.es|hol\.es|sweddy\.com|myjino\.ru|96\.lt|ow\.ly', self.url)
+            ip_address = socket.gethostbyname(self.domain)
+            ip_match = re.search('146\.112\.61\.108|213\.174\.157\.151|121\.50\.168\.88|192\.185\.217\.116|78\.46\.211\.158|181\.174\.165\.13|46\.242\.145\.103|121\.50\.168\.40|83\.125\.22\.219|46\.242\.145\.98|'
+                                '107\.151\.148\.44|107\.151\.148\.107|64\.70\.19\.203|199\.184\.144\.27|107\.151\.148\.108|107\.151\.148\.109|119\.28\.52\.61|54\.83\.43\.69|52\.69\.166\.231|216\.58\.192\.225|'
+                                '118\.184\.25\.86|67\.208\.74\.71|23\.253\.126\.58|104\.239\.157\.210|175\.126\.123\.219|141\.8\.224\.221|10\.10\.10\.10|43\.229\.108\.32|103\.232\.215\.140|69\.172\.201\.153|'
+                                '216\.218\.185\.162|54\.225\.104\.146|103\.243\.24\.98|199\.59\.243\.120|31\.170\.160\.61|213\.19\.128\.77|62\.113\.226\.131|208\.100\.26\.234|195\.16\.127\.102|195\.16\.127\.157|'
+                                '34\.196\.13\.28|103\.224\.212\.222|172\.217\.4\.225|54\.72\.9\.51|192\.64\.147\.141|198\.200\.56\.183|23\.253\.164\.103|52\.48\.191\.26|52\.214\.197\.72|87\.98\.255\.18|209\.99\.17\.27|'
+                                '216\.38\.62\.18|104\.130\.124\.96|47\.89\.58\.141|78\.46\.211\.158|54\.86\.225\.156|54\.82\.156\.19|37\.157\.192\.102|204\.11\.56\.48|110\.34\.231\.42', ip_address)
+            if url_match:
+                return -1
+            elif ip_match:
+                return -1
+            return 1
+        except:
+            return 1
+
+    def getFeaturesList(self):
+        return self.features
